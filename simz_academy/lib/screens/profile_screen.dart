1762c63810abb5ade0aa58a5595af94b93d4bec4
Christopher J Neelankavil <144330518+christopherjneelankavil@users.noreply.github.com>
2024-07-29 10:01:51 +0530

    Update profile_screen.dart

--- simz_academy/lib/screens/profile_screen.dart
+++ simz_academy/lib/screens/profile_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text('Profile'),
+      ),
       body: Center(
-        child: Text('Profile'),
+        child: Text('Profile Screen Content'),
       ),
     );
   }
