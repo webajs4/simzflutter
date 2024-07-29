aa994254a2c12ad05514ef6440c6f0e2ff77852a
Christopher J Neelankavil <144330518+christopherjneelankavil@users.noreply.github.com>
2024-07-29 10:35:01 +0530

    Update profile_screen.dart

--- simz_academy/lib/screens/profile_screen.dart
+++ simz_academy/lib/screens/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart'; // Ensure you have the iconsax package added in pubspec.yaml
+import 'package:simz_academy/UIHelper/home_ui_helper.dart'; // Ensure this path is correct
 
 class ProfileScreen extends StatelessWidget {
   @override
@@ -6,12 +8,20 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: Icon(Iconsax.arrow_square_left),
+          color: Color.fromRGBO(56, 15, 67, 1),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
-        title: Text('Profile'),
+        title: Center(
+          child: HomeUiHelper().customText(
+            'Sheets', 
+            24, 
+            FontWeight.w400, 
+            Color.fromRGBO(56, 15, 67, 1)
+          ),
+        ),
       ),
       body: Center(
         child: Text('Profile Screen Content'),
