1a1f86b98452bc6a2e4e2d5bd6ee4aa58a59be7b
christopherjneelankavil <christopherneelankavil@gmail.com>
2024-08-02 20:35:53 +0530

    chore: Refactor MusicLibraryScreen with improved UI and search functionality

--- simz_academy/lib/screens/music_library.dart
+++ simz_academy/lib/screens/music_library.dart
@@ -16,42 +16,55 @@ class MusicLibraryScreenState extends State<MusicLibraryScreen> {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: const Icon(Iconsax.arrow_square_left, color:Color.fromRGBO(56, 15, 67, 1)),
+          icon: const Icon(Iconsax.arrow_square_left,
+              color: Color.fromRGBO(56, 15, 67, 1)),
           onPressed: () {
-            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context){
-              return  BottomNav();
+            Navigator.of(context)
+                .pushReplacement(MaterialPageRoute(builder: (context) {
+              return BottomNav();
             }));
           },
         ),
-        title: Center(child: HomeUiHelper().customText('Recorded Classes', 24, FontWeight.w400, Color.fromRGBO(56, 15, 67, 1))),
+        title: Center(
+            child: HomeUiHelper().customText('Recorded Classes', 24,
+                FontWeight.w400, Color.fromRGBO(56, 15, 67, 1))),
       ),
-      body: Column(
-        children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 10.0,bottom: 10.0,left: 12.0,right: 12.0),
-              child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search Classes',
-                hintStyle: TextStyle(
+      body: Column(children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(
+              top: 10.0, bottom: 10.0, left: 12.0, right: 12.0),
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: 'Search Classes',
+              hintStyle: TextStyle(
                 color: Color.fromRGBO(205, 140, 230, 1),
-                ),
-                prefixIcon: Icon(
+              ),
+              prefixIcon: Icon(
                 Iconsax.search_normal,
                 color: Color.fromRGBO(205, 140, 230, 1),
-                ),
-                border: OutlineInputBorder(
+              ),
+              border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(10),
                 borderSide: BorderSide(
                   color: Color.fromRGBO(246, 235, 252, 1),
                 ),
-                ),
-                fillColor: Color.fromRGBO(246, 235, 252, 1),
-                filled: true,
               ),
-                        ),
+              fillColor: Color.fromRGBO(246, 235, 252, 1),
+              filled: true,
             ),
-        ]
-      ),
+          ),
+        ),
+        Container(
+          width: 365,
+          height: 101,
+          decoration: ShapeDecoration(
+            color: Color(0xFF813299),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(17),
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
